110bab4c0e3030bece90802a126d15cf0634a2ea
Naohiro Sakuma <nao.bear@gmail.com>
2013-09-09 02:20:55 +0900

    modified vim description.

--- lib/mytool/command.rb
+++ lib/mytool/command.rb
@@ -4,7 +4,7 @@ require_relative "command/zsh"
 
 module Mytool
   class Command < Thor
-    register(Mytool::Vim, 'vim', 'vim [COMMAND]', 'subcommand')
+    register(Mytool::Vim, 'vim', 'vim [COMMAND]', 'Vim controll')
     register(Mytool::Zsh, 'zsh', 'zsh [COMMAND]', 'Zsh controll')
   end
 end
