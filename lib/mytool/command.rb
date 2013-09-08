62bb96f4fb5bc4d406f3552609daffe488a16656
Naohiro Sakuma <nao.bear@gmail.com>
2013-09-09 02:21:18 +0900

    Add subcommand all set up.

--- lib/mytool/command.rb
+++ lib/mytool/command.rb
@@ -4,6 +4,11 @@ require_relative "command/zsh"
 
 module Mytool
   class Command < Thor
+    desc 'setup_all', "setting up all."
+    def setup_all
+      invoke 'vim:install'
+      invoke 'zsh:install'
+    end
     register(Mytool::Vim, 'vim', 'vim [COMMAND]', 'Vim controll')
     register(Mytool::Zsh, 'zsh', 'zsh [COMMAND]', 'Zsh controll')
   end
