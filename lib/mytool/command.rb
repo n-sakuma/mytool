7b2d6326289c8537ae32cf1cc68a1d93f0999045
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-09-09 12:16:07 +0900

    Add command devenv_gems

--- lib/mytool/command.rb
+++ lib/mytool/command.rb
@@ -2,6 +2,7 @@ require 'thor'
 require_relative "command/vim"
 require_relative "command/zsh"
 require_relative "command/tmux"
+require_relative "command/devenv_gems"
 
 module Mytool
   class Command < Thor
@@ -10,6 +11,7 @@ module Mytool
       invoke 'vim:install'
       invoke 'zsh:install'
       invoke 'tmux:install'
+      invoke 'gems:install'
     end
 
     desc 'update_all', "update setting"
@@ -17,10 +19,12 @@ module Mytool
       invoke 'vim:update'
       invoke 'zsh:update'
       invoke 'tmux:update'
+      invoke 'gems:update'
     end
 
     register(Mytool::Vim, 'vim', 'vim [COMMAND]', 'Vim controll')
     register(Mytool::Zsh, 'zsh', 'zsh [COMMAND]', 'Zsh controll')
     register(Mytool::Tmux, 'tmux', 'tmux [COMMAND]', 'Tmux controll')
+    register(Mytool::DevenvGems, 'gems', 'gems [COMMAND]', 'Dev env gems controll')
   end
 end
